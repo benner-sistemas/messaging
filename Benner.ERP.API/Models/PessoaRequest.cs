8e421ce3d996bb56507c40baf64c667b84935670
sanguedemonstro <sanguedemonstro@gmail.com>
2019-12-04 15:08:31 -0300

    1723990- exemplo inicial e inacabado de um producer

---
+++ Benner.ERP.API/Models/PessoaRequest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Benner.ERP.API.Models
+{
+    public interface IEnterpriseIntegrationResquest
+    {
+        Guid? RequestID { get; set; }
+    }
+
+    public class PessoaRequest : IEnterpriseIntegrationResquest
+    {
+        [Key]
+        public Guid? RequestID { get; set; }
+
+        [Required]
+        [StringLength(14)]
+        public string CPF { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        public DateTime? Nascimento { get; set; }
+
+        [Required]
+        public Endereco Endereco { get; set; }
+    }
+
+    public class Endereco
+    {
+        [Required]
+        public string Logradouro { get; set; }
+
+        [Required]
+        public int? Numero { get; set; }
+
+        [Required]
+        [StringLength(9)]
+        public string CEP { get; set; }
+
+        [Required]
+        public string Bairro { get; set; }
+
+        [Required]
+        public string Municipio { get; set; }
+
+        [Required]
+        public string Estado { get; set; }
+    }
+}
