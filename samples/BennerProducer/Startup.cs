977e77792d9ec511cbcad5f514f030d93a9a162c
nathan.kruger <nathan.kruger@benner.com.br>
2019-12-16 17:09:06 -0300

    redirect https no startup.cs

--- samples/BennerProducer/Startup.cs
+++ samples/BennerProducer/Startup.cs
@@ -19,6 +19,10 @@ namespace BennerProducer
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddHttpsRedirection(options =>
+            {
+                options.HttpsPort = 5672;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,7 +36,7 @@ namespace BennerProducer
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-            }
+            }   
 
             app.UseHttpsRedirection();
             app.UseMvc();
