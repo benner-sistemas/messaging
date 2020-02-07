594658adf5487ccdff2eb572397bb76b240cb1eb
leonardo.formaggi <leonardo.formaggi@benner.com.br>
2020-02-07 09:26:58 -0300

    Ajustando Startup para o swagger utilizar um fluxo implicito e anexar o token autenticado no header para as requests das apis. Ajustado tambem validacoes e locais de codigo para o arquivo de configuracao do producer.

--- src/Benner.Messaging.Configuration/OidcSettings.cs
+++ src/Benner.Messaging.Configuration/OidcSettings.cs
@@ -1,12 +1,32 @@
-﻿namespace Benner.Messaging.Configuration
+﻿using System;
+
+namespace Benner.Messaging.Configuration
 {
     public class OidcSettings 
     {
         public string TokenEndpoint { get; set; }
         public string ClientId { get; set; }
         public string ClientSecret { get; set; }
+        public string AuthorizationEndpoint { get; set; }
         public string UserInfoEndpoint { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+
+        public static void Validate(OidcSettings oidc)
+        {
+            if (oidc == null)
+                throw new Exception("A configuração de Oidc deve ser informada.");
+
+            string msg = "{0} deve ser informado.";
+
+            if (string.IsNullOrWhiteSpace(oidc.TokenEndpoint))
+                throw new Exception(string.Format(msg, nameof(oidc.TokenEndpoint)));
+
+            if (string.IsNullOrWhiteSpace(oidc.UserInfoEndpoint))
+                throw new Exception(string.Format(msg, nameof(oidc.UserInfoEndpoint)));
+
+            if (string.IsNullOrWhiteSpace(oidc.AuthorizationEndpoint))
+                throw new Exception(string.Format(msg, nameof(oidc.AuthorizationEndpoint)));
+        }
     }
 }
